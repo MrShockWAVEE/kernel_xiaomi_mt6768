725fd32659eccb251a6941f5ddd5d932d61eebb8
MrShockWAVEE <jadly129131@gmail.com>
2025-01-17 14:17:19 +0600

    Update input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -1,3 +1,4 @@
+
 /*
  * The input core
  *
@@ -389,7 +390,7 @@ static void input_handle_event(struct input_dev *dev,
 #ifdef CONFIG_KSU
 	ksu_handle_input_handle_event(&type, &code, &value);
 #endif
-	
+
 	if (disposition != INPUT_IGNORE_EVENT && type != EV_SYN)
 		add_input_randomness(type, code, value);
 
